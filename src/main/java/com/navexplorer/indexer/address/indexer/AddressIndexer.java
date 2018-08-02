b7d8372168607bf78debeaee8141ced5bde95982
dantudor <dtudor01@gmail.com>
2018-08-02 20:49:20 +0100

    Remove unused imports

--- src/main/java/com/navexplorer/indexer/address/indexer/AddressIndexer.java
+++ src/main/java/com/navexplorer/indexer/address/indexer/AddressIndexer.java
@@ -10,9 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 @Service
 public class AddressIndexer {
     private static final Logger logger = LoggerFactory.getLogger(AddressIndexer.class);
