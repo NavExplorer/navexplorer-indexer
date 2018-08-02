fdbe8e386dd65f211de32d2e4ad97ed79a66b283
dantudor <dtudor01@gmail.com>
2018-08-02 14:05:52 +0100

    AddressIndexerTest

--- src/main/java/com/navexplorer/indexer/address/indexer/AddressIndexer.java
+++ src/main/java/com/navexplorer/indexer/address/indexer/AddressIndexer.java
@@ -21,6 +21,9 @@ public class AddressIndexer {
     private BlockTransactionService blockTransactionService;
 
     @Autowired
+    private AddressExtractor addressExtractor;
+
+    @Autowired
     private AddressTransactionFactory addressTransactionFactory;
 
     @Autowired
@@ -30,7 +33,7 @@ public class AddressIndexer {
         logger.info(String.format("Indexing addresses for block: %s", block.getHeight()));
 
         blockTransactionService.getByHeight(block.getHeight()).forEach(
-                blockTransaction -> getAllTransactionAddresses(blockTransaction).forEach(
+                blockTransaction -> addressExtractor.getAllAddressesFromBlockTransaction(blockTransaction).forEach(
                         address -> {
                             AddressTransaction addressTransaction = addressTransactionFactory.create(address, blockTransaction);
                             if (addressTransaction != null) {
@@ -39,14 +42,5 @@ public class AddressIndexer {
                         }
                 )
         );
-    }
-
-    private Set<String> getAllTransactionAddresses(BlockTransaction blockTx) {
-        Set<String> addresses = new HashSet<>();
-
-        blockTx.getInputs().forEach(i -> i.getAddresses().stream().collect(Collectors.toCollection(() -> addresses)));
-        blockTx.getOutputs().forEach(o -> o.getAddresses().stream().collect(Collectors.toCollection(() -> addresses)));
-
-        return addresses;
     }
 }
