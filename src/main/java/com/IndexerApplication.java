e6c7b30ccc61e2d604acae0fe768a612604881ec
dantudor <dtudor01@gmail.com>
2018-12-09 21:17:11 +0000

    COld Staking almost done

--- src/main/java/com/IndexerApplication.java
+++ src/main/java/com/IndexerApplication.java
@@ -47,6 +47,7 @@ public class IndexerApplication implements CommandLineRunner {
 
         softForkImporter.importSoftForks();
 
+        blockRewinder.rewindToHeight(800L);
         blockRewinder.rewindTop10Blocks();
         blockIndexer.indexAllBlocks();
 
