2d6d1927305119eaf6c4899444d3a90f62ccfa9a
dantudor <dtudor01@gmail.com>
2019-02-03 12:26:01 +0000

    feature z

--- src/main/java/com/navexplorer/indexer/block/factory/BlockTransactionFactory.java
+++ src/main/java/com/navexplorer/indexer/block/factory/BlockTransactionFactory.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class BlockTransactionFactory {
@@ -70,17 +71,21 @@ public class BlockTransactionFactory {
 
     private Double applyStaking(BlockTransaction transaction) {
         if (!transaction.isCoinbase() && transaction.getOutputAmount() - transaction.getInputAmount() > 0) {
-            String stakingAddress = transaction.getOutputs().stream()
+            Output stakingOutput = transaction.getOutputs().stream()
                     .filter(t -> t.getAddresses().size() != 0)
-                    .findFirst().orElse(new Output()).getAddresses().get(0);
+                    .findFirst().orElse(new Output());
 
-            if (!transaction.hasInputWithAddress(stakingAddress)) {
-                transaction.getInputs().forEach(i -> i.getAddresses().add(stakingAddress));
+            if (stakingOutput.getAddresses().size() != 0) {
+                String stakingAddress = stakingOutput.getAddresses().get(0);
+
+                if (!transaction.hasInputWithAddress(stakingAddress)) {
+                    transaction.getInputs().forEach(i -> i.getAddresses().add(stakingAddress));
+                }
+
+                return transaction.getOutputs().stream()
+                        .filter(t -> t.getAddresses().contains(stakingAddress))
+                        .mapToDouble(Output::getAmount).sum() - transaction.getInputAmount();
             }
-
-            return transaction.getOutputs().stream()
-                    .filter(t -> t.getAddresses().contains(stakingAddress))
-                    .mapToDouble(Output::getAmount).sum() - transaction.getInputAmount();
         }
 
         return 0.0;
