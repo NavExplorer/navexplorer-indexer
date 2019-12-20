fb4c879a9545eb9f3e1114db1158b9d2f86b3267
Dan Tudor <dtudor01@gmaiol.com>
2019-12-20 22:44:01 +0000

    Add log for reached best block

--- src/main/java/com/navexplorer/indexer/block/indexer/BlockIndexer.java
+++ src/main/java/com/navexplorer/indexer/block/indexer/BlockIndexer.java
@@ -84,6 +84,8 @@ public class BlockIndexer {
             return indexBlock(apiBlock, previousBalance);
         } catch (OrphanBlockException e) {
             applicationEventPublisher.publishEvent(new OrphanedBlockEvent(this));
+        } catch (ReachedBestBlockException e) {
+            logger.info("Reached the best block");
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
